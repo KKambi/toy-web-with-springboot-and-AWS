d1ea755fdf845821ffee90290acb9bd481be97c5
KKambi <37494867+KKambi@users.noreply.github.com>
2020-02-08 21:15:48 +0900

    Add: Post API Update test

--- src/test/java/com/kkambi/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/kkambi/book/springboot/web/PostsApiControllerTest.java
@@ -3,6 +3,7 @@ package com.kkambi.book.springboot.web;
 import com.kkambi.book.springboot.domain.posts.Posts;
 import com.kkambi.book.springboot.domain.posts.PostsRepository;
 import com.kkambi.book.springboot.web.dto.PostsSaveRequestDto;
+import com.kkambi.book.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -59,6 +62,47 @@ public class PostsApiControllerTest {
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
 
+    @Test
+    public void Posts_수정된다() throws Exception {
+        //given
+        //1. Post entity를 repository(dao)를 통해 생성한다
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //2. Update할 정보는 다음과 같다.
+        Long updateId = savedPosts.getId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        //3. requestDto를 통해 업데이트를 요청하기 위해, PostsRequestDto 객체를 생성한다.
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        //4. 업데이트API의 URL은 다음과 같다.
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        //5. dto를 담은 request 객체를 생성한다
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        //6. api요청을 보내고 reponse 객체에 결과를 담는다
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        //then
+        //7. 정상적인 응답인지 StatusCode, Body크기를 통해 확인한다
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        //8. 값을 비교한다
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
 }
