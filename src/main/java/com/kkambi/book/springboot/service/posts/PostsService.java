54e778b2fb922d9f8d1319b8ea532fb8ec85f0d5
KKambi <37494867+KKambi@users.noreply.github.com>
2020-02-13 22:56:39 +0900

    Add: SELECT all posts code in Repo / Service / Dto / Controller

--- src/main/java/com/kkambi/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/kkambi/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.kkambi.book.springboot.service.posts;
 
 import com.kkambi.book.springboot.domain.posts.Posts;
 import com.kkambi.book.springboot.domain.posts.PostsRepository;
+import com.kkambi.book.springboot.web.dto.PostsListResponseDto;
 import com.kkambi.book.springboot.web.dto.PostsResponseDto;
 import com.kkambi.book.springboot.web.dto.PostsSaveRequestDto;
 import com.kkambi.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,5 +40,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
